b77dbb7d6a5e448809e2e6eff2e7420e68a5dd2a
Dane Smith <dsmith@Danes-iMac.local>
2017-11-27 21:24:17 -0600

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,8 +21,8 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
-      cordova.plugins.Keyboard.disableScroll(true);
+      window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+      window.cordova.plugins.Keyboard.disableScroll(true);
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
